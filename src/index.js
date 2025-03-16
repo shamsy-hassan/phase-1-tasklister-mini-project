862396c5caf1d5b70471c3bd959f7a8396b6f874
shamsy-hassan <shamsyhassan254@gmail.com>
2025-03-16 23:45:10 +0300

    assignent

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const newTaskForm = document.querySelector('form');
+  newTaskForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const newToDo = e.target.querySelector('#new-task-description').value
+    buildToDo(newToDo);
+    newTaskForm.reset();
+  })
 });
+
+  function buildToDo(newToDo){
+    const taskLi = document.createElement('li');
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'x';
+
+    taskLi.textContent = newToDo;
+    taskLi.appendChild(deleteButton);
+    document.querySelector('#tasks').appendChild(taskLi);
+  
+    deleteButton.addEventListener('click', handleDelete);
+  }
+
+  function handleDelete(e){
+    e.target.parentNode.remove();
+  }
